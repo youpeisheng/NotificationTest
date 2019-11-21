2064c543f30626fef68ffb71482959a55850fd32
youpeisheng <1426890389@qq.com>
2019-11-21 16:52:25 +0800

    基本调试完毕

--- app/src/main/java/com/example/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -7,22 +7,29 @@ import androidx.core.app.NotificationCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    String ChannelId="MyID";
+    String ChannelName="channel_name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button sendNotice=(Button) findViewById(R.id.send_notice);
         sendNotice.setOnClickListener(this);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -30,19 +37,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.send_notice:
+                //设置启动的活动pendingIntent
+                Intent intent =new Intent(this,NotificationActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(this,0,intent,0);
+                //设置通知
                 NotificationManager manager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                String ChannelId="1";
-                String ChannelName="channel_name";
                 NotificationChannel channel = new NotificationChannel(ChannelId, ChannelName, NotificationManager.IMPORTANCE_HIGH);
-                manager.createNotificationChannel(channel);
+                    manager.createNotificationChannel(channel);
 
                 Notification notification= new NotificationCompat.Builder(this)
                         .setContentTitle("This is content title")
-                        .setContentText("This is content text")
+                        .setContentText("This is content text ")
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                         .setChannelId(ChannelId)
+                        .setContentIntent(pi)
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)
+                        .setAutoCancel(true)
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)))
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
                         .build();
                 manager.notify(1,notification);
                 break;
